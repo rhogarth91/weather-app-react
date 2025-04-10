0812ba19657044379aa07253f4f0bcb00074e4f1
rhogarth91 <148714164+rhogarth91@users.noreply.github.com>
2025-04-10 14:11:06 +0100

    h1 amended

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather App</h1>
+        <h1>Weather Application</h1>
         <Weather />
       </header>
 
