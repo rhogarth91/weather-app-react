c862048c2a4d9ef680bcec86bab9ff9996f3d516
rhogarth91 <148714164+rhogarth91@users.noreply.github.com>
2025-04-21 14:06:57 +0100

    container created for bootstrap to work

--- src/App.js
+++ src/App.js
@@ -6,41 +6,43 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather Application</h1>
-        <Weather />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather Application</h1>
+          <Weather />
+        </header>
 
-      <footer className="App-Footer">
-        <p>
-          This project was coded by{" "}
-          <a
-            href="https://github.com/rhogarth91"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Rose H{" "}
-          </a>{" "}
-          🌹 and is
-          <a
-            href="https://github.com/rhogarth91/weather-app-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            open-sourced on GitHub{" "}
-          </a>
-          and{" "}
-          <a
-            href="https://rh-weather-app-react.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            hosted on Netlify
-          </a>
-        </p>
-      </footer>
+        <footer className="App-Footer">
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/rhogarth91"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Rose H{" "}
+            </a>{" "}
+            🌹 and is
+            <a
+              href="https://github.com/rhogarth91/weather-app-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              open-sourced on GitHub{" "}
+            </a>
+            and{" "}
+            <a
+              href="https://rh-weather-app-react.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              hosted on Netlify
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
